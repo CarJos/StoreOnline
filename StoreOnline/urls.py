95e5a2b6b833d1ba4b64cae56256cd3d54e170f5
CarJos <carlos.sanchez.p.1992@gmail.com>
2022-02-16 17:52:07 -0500

    Se crea el template users/login y se actualizan las urls, las views y el index

--- StoreOnline/urls.py
+++ StoreOnline/urls.py
@@ -20,5 +20,6 @@ from StoreOnline import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('usuarios/login',views.login_view, name='login'),
     path('admin/', admin.site.urls),
 ]
--- StoreOnline/views.py
+++ StoreOnline/views.py
@@ -1,5 +1,8 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 def index(request):
     context = {
@@ -12,3 +15,16 @@ def index(request):
         ],
     }
     return render(request, 'index.html', context)
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, 'Bienvenido {}'.format(user.username))
+            return redirect('index')
+        else:
+            messages.error(request, 'Usuario o contraseñas no validos')
+    return render(request, 'users/login.html', {})
